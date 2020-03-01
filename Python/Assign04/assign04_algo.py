10df24628bf8bae7ceaddf77ec0e52b188a18197
Sindvero <aurelien.benoist@yahoo.fr>
2020-03-01 15:00:58 -0600

    Assign04: read header and write new image

---
+++ Python/Assign04/assign04_algo.py
@@ -0,0 +1,50 @@
+import imghdr
+import subprocess
+
+
+def infoBMPImage(image):
+    imageType = imghdr.what(image) # library used to chek bmp file: https://docs.python.org/3.6/library/imghdr.html
+    infoDict = {}
+    exifToolPath = 'exiftool' # command to read the meta data of an image: https://exiftool.org/ 
+
+# print(imageType)
+
+    if (imageType == 'bmp'):
+       print("Your image is a BMP file!")
+
+    else:
+     print("Please provide a BMP file")
+
+    # use Exif tool to get the metadata 
+    process = subprocess.Popen([exifToolPath,image],stdout=subprocess.PIPE, stderr=subprocess.STDOUT,universal_newlines=True) # Pipe and redirection to store tha data
+    # get the tags in dict
+    for tag in process.stdout:
+        line = tag.strip().split(':')
+        infoDict[line[0].strip()] = line[-1].strip()
+
+    if (infoDict["BMP Version"] == "Windows V5"):
+        print("Your DIB Header version is BITMAPV5HEADER!")
+    else:
+        print("Your DIB Header version is: " + infoDict["BMP Version"] + ", it\'s not the correct version!") # Header info: 
+                                                                                                             # https://en.wikipedia.org/wiki/BMP_file_format#Bitmap_file_header
+                                                                                                             # https://docs.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-bitmapv5header
+
+    # Another solution would have been to read the data with open(image, 'rb') (the first one I use) 
+    # But the solution with the exiftool command is more simple to read the data aftermath     
+    
+    print("The file size is: " + infoDict["File Size"])
+    print("The image dimensions are: {0}x{1}".format(infoDict["Image Width"], infoDict["Image Height"]))
+                                                                                                    
+def writeNewBMP(imageSource):
+    inputImage = open(imageSource, 'rb')
+    bufferInfo = inputImage.read()
+    inputImage.close()
+
+    outputImage = open('Python/Assign04/output_image.bmp', 'wb')
+    outputImage.write(bufferInfo)
+    outputImage.close()
+    
+
+
+infoBMPImage('Python/Assign04/ballons_rgb.bmp')
+writeNewBMP('Python/Assign04/ballons_rgb.bmp')
